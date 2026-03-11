c413e5ff92c452dc0759e2420e72a5d271ffbf41
Your Name <harshitdagar913@gmail.com>
2026-03-11 16:09:01 +0530

    Add routing for women category and item details
    - Updated package.json to include react-router-dom dependency.
    - Refactored App component to implement routing using react-router.
    - Created WomenCategory component to display a list of items with links.
    - Added ItemDetail component to show details for selected items.
    - Implemented a Layout component for consistent navigation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
-
 import React from "react";
-import './../styles/App.css';
+import "../styles/App.css";
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from "react-router-dom";
 
-const App = () => {
+
+function ItemDetail() {
+  const { categoryItem } = useParams();
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>{categoryItem}</p>
     </div>
   )
 }
 
-export default App
+function WomenCategory() {
+  const itemList = ['Grooming', 'Shirt', 'Trouser', 'Jewellery'];
+  return (
+    <div>
+      <p>Women Items:</p>
+      <ul>
+        {itemList.map((item) => (
+          <li key={item}>
+            <Link to={item}>{item}</Link>
+          </li>
+        ))}
+      </ul>
+      <Outlet />
+    </div>
+  )
+}
+
+function Layout() {
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/women">Women</Link></li>
+      </ul>
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return null;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="women" element={<WomenCategory />}>
+            <Route path=":categoryItem" element={<ItemDetail />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
